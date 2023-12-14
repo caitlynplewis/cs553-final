b706e0fa4fcea7f5de29bcfaf0af827d34e4a9f2
Caitlyn Lewis <cookcai@oregonstate.edu>
2023-12-13 21:08:38 -0800

    Add checkboxes for press selection

--- wafer.py
+++ wafer.py
@@ -40,67 +40,86 @@ def clear_annotations(annots):
 
 def matplot_only_heatmap(data):
     fig, ax = plt.subplots()
-    subsets, subset_visible = subset_data(data)
+    _ , active_colors = subset_data(data)
+    active_presses = data['WP_NAME'].unique().tolist()
     rows = sorted(data['WAFER_ROW'].unique())
     cols = sorted(data['WAFER_COLUMN'].unique())
     z = count_die(data, rows, cols)
     zmax = z.max().max()
+    annots = []
+    im = None
 
+    active_data = data
     display_data = z
 
-    im = ax.imshow(display_data)
-    fig.colorbar(im, ticks=range(0,zmax,5))
-    annots = annotate_map(display_data, rows, cols, ax)
-    ax.set_aspect(3)
-
-    ####################### Radio button for cmap
-    ax_radio = plt.axes([0.0, 0.0, 0.1, 0.25])
-    labels = ["viridis", "hot", "plasma", "inferno", "magma", "cividis" ]
-    color_button = RadioButtons(ax_radio, labels, activecolor='black')
-
-    def select_color(label):
-        im.set_cmap(label)
+    def draw_plot():
+        nonlocal im, annots
+        clear_annotations(annots)
+        im = ax.imshow(display_data)
+        annots = annotate_map(display_data, rows, cols, ax)
+        ax.set_aspect(4.25)
         fig.canvas.draw()
 
-    color_button.on_clicked(select_color)
-    ####################### 
+    draw_plot()
+    ####################### Checkboxes for press selection
+    activated = [True] * (len(data["WP_NAME"].unique()))
+    labels = data["WP_NAME"].unique()
+    ax_press = plt.axes([0.0, 0.0, 0.1, 0.3])
+    press_picker = CheckButtons(ax_press , labels, activated)
+    def press_filter(label):
+        nonlocal active_data, display_data
+        if label in active_presses:
+            active_presses.remove(label)
+        else:
+            active_presses.append(label)
+        active_data = all_filters(data, active_presses, active_colors)
 
+        display_data = count_die(active_data, rows, cols)
+        draw_plot()
+
+    press_picker.on_clicked(press_filter)
     ####################### Checkboxes for data selection
     activated = [True] * (len(data["PEN_COLOR_NAME"].unique()))
     labels = data["PEN_COLOR_NAME"].unique()
     ax_check = plt.axes([0.0, 0.3, 0.1, 0.3])
     pen_colors = CheckButtons(ax_check, labels, activated)
 
-    def filter_data(label):
-        nonlocal annots
-        nonlocal im
-        clear_annotations(annots)
-        subset_visible[label] = not subset_visible[label]
-        active_subsets = []
-        for k in subsets.keys():
-            if subset_visible[k]:
-                active_subsets.append(subsets[k])
+    def color_filter(label):
+        nonlocal display_data, active_data
+        active_colors[label] = not active_colors[label]
+        active_data = all_filters(data, active_presses, active_colors)
+        display_data = count_die(active_data, rows, cols)
 
-        display_data = pd.concat(active_subsets)
-        display_data = count_die(display_data, rows, cols)
+        draw_plot()
 
-        annots = annotate_map(display_data, rows, cols, ax)
-        im = ax.imshow(display_data, vmin=0, vmax=zmax)
-        ax.set_aspect(3)
-        fig.canvas.draw()
-
-    pen_colors.on_clicked(filter_data)
+    pen_colors.on_clicked(color_filter)
     ####################### 
 
     plt.show()
 
+def all_filters(data, active_presses, active_colors):
+    active_data = filter_for_press(data, active_presses)
+    subsets, _ = subset_data(active_data)
+    active_subsets = filter_for_color(subsets, active_colors)
+    active_data = pd.concat(active_subsets)
+    return active_data
+
+def filter_for_press(data, presses):
+    output = []
+    for press in presses:
+        output.append(data[data['WP_NAME'] == press])
+    output = pd.concat(output)
+    return output
+
+def filter_for_color(subsets, subset_visible):
+    active_subsets = []
+    for k in subsets.keys():
+        if subset_visible[k]:
+            active_subsets.append(subsets[k])
+    return active_subsets
+
 def main():
     data = load_data()
-    print(data['WP_NAME'].unique())
-    # press = input("Choose a press: ")
-    press = "RS31" # For ease of development
-
-    pressdata = data[data['WP_NAME'] == press]
     matplot_only_heatmap(data)
 
 
